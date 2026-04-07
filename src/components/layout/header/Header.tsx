5e3f665b5aac1a074bbaef017a82eb788256909f
IvanJ.Dev <jivan8764@gmail.com>
2026-04-07 14:14:59 -0300

    fix: mostrar/ocultar elementos del header según la ruta

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -6,6 +6,7 @@ import { ModalSkeleton } from "../../ui/skeletons/ModalSkeleton";
 import { useModal } from "../../../context/ModalContext";
 import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "../../../services/profileApi";
+import { useLocation } from "react-router-dom";
 
 const navLinks = [
   { name: "Perfil", href: "#perfil" },
@@ -18,6 +19,10 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { openModal } = useModal();
   const modalOpenedWithSkeleton = useRef(false);
+
+  const path = useLocation();
+
+  console.log(path);
 
   const { data: profile, isLoading } = useQuery({
     queryKey: ["profile"],
@@ -262,20 +267,22 @@ const Header = () => {
             <Logo />
           </a>
 
-          <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-1">
-              {navLinks.map((link) => (
-                <a
-                  key={link.name}
-                  href={link.href}
-                  className="nav-link px-3 py-2 rounded-lg text-sm font-medium transition-all duration-300 relative group"
-                >
-                  {link.name}
-                  <span className="absolute bottom-0 left-1/2 -translate-x-1/2 w-0 h-0.5 bg-gradient-to-r from-[#39ff88] to-[#00ff9c] transition-all duration-300 group-hover:w-full" />
-                </a>
-              ))}
+          {path?.pathname === "/" && (
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-1">
+                {navLinks.map((link) => (
+                  <a
+                    key={link.name}
+                    href={link.href}
+                    className="nav-link px-3 py-2 rounded-lg text-sm font-medium transition-all duration-300 relative group"
+                  >
+                    {link.name}
+                    <span className="absolute bottom-0 left-1/2 -translate-x-1/2 w-0 h-0.5 bg-gradient-to-r from-[#39ff88] to-[#00ff9c] transition-all duration-300 group-hover:w-full" />
+                  </a>
+                ))}
+              </div>
             </div>
-          </div>
+          )}
 
           <div className="hidden md:block">
             <PrimaryButton className="text-sm" onClick={handleDownloadCV}>
@@ -293,56 +300,59 @@ const Header = () => {
               </PrimaryButton>
             </div>
 
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="inline-flex items-center justify-center p-2 rounded-lg text-gray-300 hover:text-white hover:bg-white/10 transition-colors duration-200"
-              aria-label="Toggle menu"
-            >
-              <svg
-                className={`w-6 h-6 transition-transform duration-300 ${isOpen ? "rotate-90" : ""}`}
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+            {path?.pathname === "/" && (
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="inline-flex items-center justify-center p-2 rounded-lg text-gray-300 hover:text-white hover:bg-white/10 transition-colors duration-200"
+                aria-label="Toggle menu"
               >
-                {isOpen ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                )}
-              </svg>
-            </button>
+                <svg
+                  className={`w-6 h-6 transition-transform duration-300 ${isOpen ? "rotate-90" : ""}`}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {isOpen ? (
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    ) : (
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    )}
+                  </svg>
+                </button>
+            )}
           </div>
         </div>
       </nav>
-
-      <div
-        className={`md:hidden overflow-hidden transition-all duration-300 ${
-          isOpen ? "max-h-64 opacity-70 bg-black" : "max-h-0 opacity-0"
-        }`}
-      >
-        <div className="px-4 py-3 space-y-2 backdrop-blur-sm">
-          {navLinks.map((link) => (
-            <a
-              key={link.name}
-              href={link.href}
-              onClick={() => setIsOpen(false)}
-              className="block px-4 py-3 rounded-lg text-gray-300 hover:text-white hover:bg-white/10 transition-colors duration-200"
-            >
-              {link.name}
-            </a>
-          ))}
+      {path?.pathname === "/" && (
+        <div
+          className={`md:hidden overflow-hidden transition-all duration-300 ${
+            isOpen ? "max-h-64 opacity-70 bg-black" : "max-h-0 opacity-0"
+          }`}
+        >
+          <div className="px-4 py-3 space-y-2 backdrop-blur-sm">
+            {navLinks.map((link) => (
+              <a
+                key={link.name}
+                href={link.href}
+                onClick={() => setIsOpen(false)}
+                className="block px-4 py-3 rounded-lg text-gray-300 hover:text-white hover:bg-white/10 transition-colors duration-200"
+              >
+                {link.name}
+              </a>
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </header>
   );
 };
