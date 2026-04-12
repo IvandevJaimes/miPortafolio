88a36f14c2702aa394fdea7693686a482eebf980
IvanJ.Dev <jivan8764@gmail.com>
2026-04-12 00:35:13 -0300

    feat: agregar ruta 404 con ErrorScreen

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import MainPage from "./pages/mainPage/MainPage";
 import ProjectPage from "./pages/projectPage/ProjectPage";
 import { ModalProvider } from "./context/ModalContext";
+import ErrorScreen from "./components/ui/errorComponents/ErrorScreen";
 
 const App = () => {
   return (
@@ -11,6 +12,15 @@ const App = () => {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/project/:id" element={<ProjectPage />} />
+          <Route
+            path="*"
+            element={
+              <ErrorScreen
+                error="404 - Página no encontrada"
+                onClick={() => window.location.href = "/"}
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </ModalProvider>
